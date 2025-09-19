a76b2e85b4cf73b153ba526f9916b79bb9a94b71
Yulian Kuncheff <yulian@kuncheff.com>
2025-09-19 23:41:00 +0200

    Minor fixes

--- Casks/bluefin-wallpapers.rb
+++ Casks/bluefin-wallpapers.rb
@@ -2,7 +2,7 @@ cask "bluefin-wallpapers" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/ublue-os/packages/archive/refs/heads/main.zip"
+  url "https://github.com/ublue-os/packages/archive/refs/heads/main.tar.gz"
   name "bluefin-wallpapers"
   desc "Wallpapers for Bluefin"
   homepage "https://github.com/ublue-os/packages/tree/main/packages/bluefin/wallpapers"
@@ -15,5 +15,4 @@ cask "bluefin-wallpapers" do
   Dir.glob("#{staged_path}/packages-main/packages/bluefin/wallpapers/images/*").each do |file|
     artifact file, target: "#{Dir.home}/.local/share/backgrounds/#{File.basename(file)}"
   end
-
 end
