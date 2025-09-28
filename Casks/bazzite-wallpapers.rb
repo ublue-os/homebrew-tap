a2de31e9ae32d6e816b0257e19ae2177c201215c
renner <renner0@posteo.de>
2025-09-28 21:50:39 +0200

    feat: add bazzite-wallpapers
    fixes: https://github.com/ublue-os/homebrew-tap/issues/33

---
+++ Casks/bazzite-wallpapers.rb
@@ -0,0 +1,38 @@
+cask "bazzite-wallpapers" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/ublue-os/bazzite/tree/main/press_kit/art"
+  name "bazzite-wallpapers"
+  desc "Bazzite Wallpapers"
+  homepage "https://bazzite.gg"
+
+  wallpapers = [
+    {
+      filename: "Convergence_Wallpaper.png",
+      url: "https://raw.githubusercontent.com/ublue-os/bazzite/main/press_kit/art/Convergence_Wallpaper.png"
+    },
+    {
+      filename: "Convergence_Wallpaper_DX.jxl",
+      url: "https://raw.githubusercontent.com/ublue-os/bazzite/main/press_kit/art/Convergence_Wallpaper_DX.jxl"
+    },
+    {
+      filename: "Bazzite_Giants.jpg",
+      url: "https://raw.githubusercontent.com/ublue-os/bazzite/main/press_kit/art/Bazzite_Giants.jpg"
+    }
+  ]
+
+  destination_dir = "#{Dir.home}/.local/share/backgrounds/bazzite"
+
+  stage_only true
+
+  postflight do
+    system "mkdir", "-p", destination_dir
+
+    wallpapers.each do |wallpaper|
+      ohai "Downloading #{wallpaper[:filename]} to #{destination_dir}"
+      system "curl", "-L", "-o", "#{destination_dir}/#{wallpaper[:filename]}", wallpaper[:url]
+    end
+  end
+end
+
