e09b0c2a9d8eedd73765646fd86576471a859342
Yulian Kuncheff <yulian@kuncheff.com>
2025-10-16 23:54:07 +0200

    framework-wallpapers 2025-10-16

--- Casks/framework-wallpapers.rb
+++ Casks/framework-wallpapers.rb
@@ -1,5 +1,5 @@
 cask "framework-wallpapers" do
-  version "2025-10-02"
+  version "2025-10-16"
   sha256 :no_check
 
   url "https://github.com/projectbluefin/artwork/releases/latest/download/framework-wallpapers.tar.zstd"
