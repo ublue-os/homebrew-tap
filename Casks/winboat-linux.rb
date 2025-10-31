33a6ec23ab9abbe7192e390331c0eb2e3f35cfb8
James Reilly <jreilly1821@gmail.com>
2025-10-31 14:07:40 +0530

    Fix winboat cask: use binary instead of app, add desktop file

--- Casks/winboat-linux.rb
+++ Casks/winboat-linux.rb
@@ -1,4 +1,5 @@
 cask "winboat-linux" do
+  arch arm: "arm64", intel: "x64"
   os linux: "linux"
   version "0.8.7"
   sha256 "e0d57d9f214b609b7db73de29034dbd684a924600aaf5238f43d151eaa6dcb72"
@@ -13,10 +14,28 @@ cask "winboat-linux" do
     strategy :github_latest
   end
 
-  app "winboat-#{version}-x64/winboat"
+  binary "winboat-#{version}-x64/winboat"
+
+  preflight do
+    FileUtils.mkdir_p "#{Dir.home}/.local/share/applications"
+    
+    desktop_file = <<~EOS
+      [Desktop Entry]
+      Name=Winboat
+      Comment=Run Windows apps on Linux with seamless integration
+      Exec=#{HOMEBREW_PREFIX}/bin/winboat %U
+      Terminal=false
+      Type=Application
+      Icon=winboat
+      Categories=Utility;
+    EOS
+
+    File.write("#{Dir.home}/.local/share/applications/winboat.desktop", desktop_file)
+  end
 
   zap trash: [
     "~/.config/winboat",
     "~/.local/share/winboat",
+    "~/.local/share/applications/winboat.desktop",
   ]
 end
