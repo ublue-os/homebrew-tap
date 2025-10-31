e149e8dc35cdc4c6b90ca432cec1ec246b0f6a09
James Reilly <jreilly1821@gmail.com>
2025-10-31 14:02:50 +0530

    Fix winboat URL: use correct tarball name -x64.tar.gz

--- Formula/winboat.rb
+++ Formula/winboat.rb
@@ -1,8 +1,8 @@
 class Winboat < Formula
   desc "Run Windows apps on Linux with seamless integration"
   homepage "https://www.winboat.app"
-  url "https://github.com/TibixDev/winboat/releases/download/v0.8.7/winboat-0.8.7.tar.gz"
-  sha256 "bf74ba69a303235d671a61b881dc42c9ce0dc99bcbcaab6713f43eb160984014"
+  url "https://github.com/TibixDev/winboat/releases/download/v0.8.7/winboat-0.8.7-x64.tar.gz"
+  sha256 "e0d57d9f214b609b7db73de29034dbd684a924600aaf5238f43d151eaa6dcb72"
   license "MIT"
 
   livecheck do
