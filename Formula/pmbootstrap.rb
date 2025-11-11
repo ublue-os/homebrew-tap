71ed6bf8639a4c641083cb30a709c176fb0f4949
Yulian Kuncheff <yulian@kuncheff.com>
2025-11-11 22:51:29 +0100

    pmbootstrap formula

---
+++ Formula/pmbootstrap.rb
@@ -0,0 +1,19 @@
+class Pmbootstrap < Formula
+  include Language::Python::Virtualenv
+
+  version "3.6.0"
+  desc "A sophisticated chroot / build / flash tool to develop and install postmarketOS'"
+  homepage "https://gitlab.postmarketos.org/postmarketOS/pmbootstrap"
+  license "GPL-3.0-or-later"
+  url "https://gitlab.postmarketos.org/postmarketOS/pmbootstrap.git", revision: "#{version}"
+
+  depends_on "python@3.14"
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    system "false"
+  end
+end
