8d25f5f976f4b66af6d278ead30b440c6832efa8
Ahmed Adan <ahmed.adan@gmail.com>
2025-12-13 17:50:36 -0500

    fix(wallpapers): use postflight for artifacts
    Artifact stanzas with Dir.glob were evaluated at cask parse time,
    before archives were extracted. This caused "already a Generic
    Artifact" errors during upgrades when old version files existed.
    
    - Move Dir.glob file linking to postflight blocks
    - Add uninstall_postflight for cleanup on uninstall
    - Add zap stanzas for complete removal

--- Casks/framework-wallpapers.rb
+++ Casks/framework-wallpapers.rb
@@ -14,41 +14,18 @@ cask "framework-wallpapers" do
   on_macos do
     url "https://github.com/ublue-os/artwork/releases/download/framework-v#{version}/framework-wallpapers-macos.tar.zstd"
     sha256 "9d69f1d59e0d20d1e91fbb8c7c6e9009cb95b06058d31b407debad77a1c82da4"
-
-    Dir.glob("#{staged_path}/*").each do |file|
-      artifact file, target: "#{Dir.home}/Library/Desktop Pictures/Framework/#{File.basename(file)}"
-    end
   end
 
   on_linux do
-    destination_dir = "#{Dir.home}/.local/share/backgrounds/framework"
-    kde_destination_dir = "#{Dir.home}/.local/share/wallpapers/framework"
-
     if File.exist?("/usr/bin/plasmashell")
       url "https://github.com/ublue-os/artwork/releases/download/framework-v#{version}/framework-wallpapers-kde.tar.zstd"
       sha256 "01225c2d24a8d14e4a42953889dcc6263050792a21f87b23346fb9ff6c13adf8"
-
-      Dir.glob("#{staged_path}/*").each do |file|
-        artifact file, target: "#{kde_destination_dir}/#{File.basename(file)}"
-      end
     elsif File.exist?("/usr/bin/gnome-shell") || File.exist?("/usr/bin/mutter")
       url "https://github.com/ublue-os/artwork/releases/download/framework-v#{version}/framework-wallpapers-gnome.tar.zstd"
       sha256 "f936e03bd0486bab1ec1fdce30f93ff81fd49f949d8c9b878d51fa58dfa96524"
-
-      Dir.glob("#{staged_path}/images/*").each do |file|
-        artifact file, target: "#{destination_dir}/#{File.basename(file)}"
-      end
-
-      Dir.glob("#{staged_path}/gnome-background-properties/*").each do |file|
-        artifact file, target: "#{Dir.home}/.local/share/gnome-background-properties/#{File.basename(file)}"
-      end
     else
       url "https://github.com/ublue-os/artwork/releases/download/framework-v#{version}/framework-wallpapers-png.tar.zstd"
       sha256 "ab55af2ddf076955c6982065e827f4c1fb9864555cb151c7bb10e8187492caf3"
-
-      Dir.glob("#{staged_path}/*").each do |file|
-        artifact file, target: "#{destination_dir}/#{File.basename(file)}"
-      end
     end
   end
 
@@ -70,8 +47,55 @@ cask "framework-wallpapers" do
 
   postflight do
     if OS.mac?
+      Dir.glob("#{staged_path}/*").each do |file|
+        target = "#{Dir.home}/Library/Desktop Pictures/Framework/#{File.basename(file)}"
+        FileUtils.ln_sf(file, target)
+      end
       puts "Wallpapers installed to: #{Dir.home}/Library/Desktop Pictures/Framework"
       puts "To use: System Settings > Wallpaper > Add Folder"
     end
+
+    if OS.linux?
+      destination_dir = "#{Dir.home}/.local/share/backgrounds/framework"
+      kde_destination_dir = "#{Dir.home}/.local/share/wallpapers/framework"
+
+      if File.exist?("/usr/bin/plasmashell")
+        Dir.glob("#{staged_path}/*").each do |file|
+          target = "#{kde_destination_dir}/#{File.basename(file)}"
+          FileUtils.ln_sf(file, target)
+        end
+      elsif File.exist?("/usr/bin/gnome-shell") || File.exist?("/usr/bin/mutter")
+        Dir.glob("#{staged_path}/images/*").each do |file|
+          target = "#{destination_dir}/#{File.basename(file)}"
+          FileUtils.ln_sf(file, target)
+        end
+
+        Dir.glob("#{staged_path}/gnome-background-properties/*").each do |file|
+          target = "#{Dir.home}/.local/share/gnome-background-properties/#{File.basename(file)}"
+          FileUtils.ln_sf(file, target)
+        end
+      else
+        Dir.glob("#{staged_path}/*").each do |file|
+          target = "#{destination_dir}/#{File.basename(file)}"
+          FileUtils.ln_sf(file, target)
+        end
+      end
+    end
   end
+
+  uninstall_postflight do
+    FileUtils.rm_r "#{Dir.home}/Library/Desktop Pictures/Framework" if OS.mac?
+
+    if OS.linux?
+      FileUtils.rm_r "#{Dir.home}/.local/share/backgrounds/framework"
+      FileUtils.rm_r "#{Dir.home}/.local/share/wallpapers/framework"
+    end
+  end
+
+  zap trash: [
+    "#{Dir.home}/.local/share/backgrounds/framework",
+    "#{Dir.home}/.local/share/gnome-background-properties/framework-*.xml",
+    "#{Dir.home}/.local/share/wallpapers/framework",
+    "#{Dir.home}/Library/Desktop Pictures/Framework",
+  ]
 end
