c13d3f12dd276d1698af508b0cdc51681425c9b1
inffy <10782843+inffy@users.noreply.github.com>
2026-04-08 04:01:33 +0000

    antigravity-linux 1.22.2

--- Casks/antigravity-linux.rb
+++ Casks/antigravity-linux.rb
@@ -1,9 +1,9 @@
 cask "antigravity-linux" do
   arch arm: "arm", intel: "x64"
 
-  version "1.21.9,4905428782546944"
-  sha256 arm64_linux:  "76d56462e0fad95a98efaf25d06be9c4789e3b8551fe5b936a6858b14c723364",
-         x86_64_linux: "85ea4d55f52d32fbbf9d92fddc747f10e8d04c1bd00a07721b571fa7f2ef5226"
+  version "1.22.2,5206900187463680"
+  sha256 arm64_linux:  "601fe7d97bf49fe06ebde331f1385e9e7e58269d23667808ae58256a87929a23",
+         x86_64_linux: "85c6b2decfefef2c6e0adaf161b602da7c1ecb2db6157ec0a4caffcfe4811209"
 
   url "https://edgedl.me.gvt1.com/edgedl/release2/j0qc3/antigravity/stable/#{version.csv.first}-#{version.csv.second}/linux-#{arch}/Antigravity.tar.gz",
       verified: "edgedl.me.gvt1.com/edgedl/release2/j0qc3/antigravity/"
