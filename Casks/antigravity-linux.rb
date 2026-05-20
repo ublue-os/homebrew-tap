4cdbbc42aeb8424b1e77391dc213b0ff25328671
James Reilly <jreilly1821@gmail.com>
2026-05-20 10:26:28 +0530

    Add antigravity-ide binary target

--- Casks/antigravity-linux.rb
+++ Casks/antigravity-linux.rb
@@ -34,6 +34,7 @@ cask "antigravity-linux" do
            target: "#{Dir.home}/.local/share/icons/hicolor/512x512/apps/antigravity.png"
 
   binary "#{staged_path}/Antigravity IDE/resources/app/bin/antigravity"
+  binary "#{staged_path}/Antigravity IDE/resources/app/bin/antigravity", target: "antigravity-ide"
   binary "#{staged_path}/Antigravity IDE/resources/app/bin/antigravity", target: "agy-ide"
 
   preflight do
