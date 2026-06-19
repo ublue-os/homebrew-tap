99249d37f43112f45e675be4b25d278d591fdad7
Cloud <17345221+nklowns@users.noreply.github.com>
2026-06-19 12:22:44 -0300

    fix(cask): vsc-insiders versioning and vscodium linting (#450)

--- Casks/vscodium-linux.rb
+++ Casks/vscodium-linux.rb
@@ -3,7 +3,6 @@ cask "vscodium-linux" do
   os linux: "linux"
 
   version "1.121.03429"
-
   sha256 arm64_linux:  "993e5dbf0f06399d069d968a452fd30334031046033a0723eb0ea534bcb9910d",
          x86_64_linux: "2c9b06735d4c1face570935f4968d358f9f6269f5d9237813d0b825c7d70a143"
 
