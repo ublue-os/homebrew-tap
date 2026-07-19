2ce577a8040bd513aed273631c5231f9518c9911
Michaël Tremblay <13114338+Tichael@users.noreply.github.com>
2026-07-19 12:41:08 -0400

    Fix Antigravity URLs (#494)
    Co-authored-by: inffy <10782843+inffy@users.noreply.github.com>

--- Casks/antigravity-cli-linux.rb
+++ Casks/antigravity-cli-linux.rb
@@ -1,7 +1,5 @@
 cask "antigravity-cli-linux" do
   arch arm: "arm", intel: "x64"
-  file_arch = on_arch_conditional arm: "arm64", intel: "x64"
-  livecheck_arch = on_arch_conditional arm: "arm64", intel: "amd64"
   os linux: "linux"
 
   version "1.0.9,6003845613092864"
@@ -10,14 +8,14 @@ cask "antigravity-cli-linux" do
          arm64_linux:  "944d67056b7cc6e4411dca84d9f077b1e460c8cd2a4432d2cdf219421ff73e5a",
          x86_64_linux: "cd80f85f43b52b389d7b498d6784f8316d57a9cc62eae23d840c5de368f9c4d5"
 
-  url "https://storage.googleapis.com/antigravity-public/antigravity-cli/#{version.csv.first}-#{version.csv.second}/linux-#{arch}/cli_linux_#{file_arch}.tar.gz",
+  url "https://storage.googleapis.com/antigravity-public/antigravity-cli/#{version.csv.first}-#{version.csv.second}/linux-#{arch}/cli_linux_#{(arch == "arm") ? "arm64" : "x64"}.tar.gz",
       verified: "storage.googleapis.com/antigravity-public/antigravity-cli/"
   name "Google Antigravity CLI"
   desc "Terminal interface for Antigravity agents"
   homepage "https://antigravity.google/product/antigravity-cli"
 
   livecheck do
-    url "https://antigravity-cli-auto-updater-974169037036.us-central1.run.app/manifests/linux_#{livecheck_arch}.json"
+    url "https://antigravity-cli-auto-updater-974169037036.us-central1.run.app/manifests/linux_#{(arch == "arm") ? "arm64" : "amd64"}.json"
     regex(%r{/antigravity-cli/([^/]+)/}i)
     strategy :json do |json, regex|
       match = json["url"]&.match(regex)
