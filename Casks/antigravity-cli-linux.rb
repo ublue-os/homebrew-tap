f10a9007b6ada7b5dd422c2f2029c30217c02872
inffy <10782843+inffy@users.noreply.github.com>
2026-07-25 04:03:01 +0000

    antigravity-cli-linux 1.1.7

--- Casks/antigravity-cli-linux.rb
+++ Casks/antigravity-cli-linux.rb
@@ -2,11 +2,11 @@ cask "antigravity-cli-linux" do
   arch arm: "arm", intel: "x64"
   os linux: "linux"
 
-  version "1.1.6,6535449645285376"
-  sha256 arm:          "78040d87f4fef72bb785af68cf7d9d6c25024710ee58f8281f85c3f1e47b975a",
-         intel:        "2448b9bb1d3496063a6335d021dcab90c42d71fdaad6346ef8a395f0ca0fe9d0",
-         arm64_linux:  "78040d87f4fef72bb785af68cf7d9d6c25024710ee58f8281f85c3f1e47b975a",
-         x86_64_linux: "2448b9bb1d3496063a6335d021dcab90c42d71fdaad6346ef8a395f0ca0fe9d0"
+  version "1.1.7,5951805767680000"
+  sha256 arm:          "0d6d488851745e80e69b8935d063e742945811b47111994b1a6dbd27df3010d5",
+         intel:        "946cd06258d0ede72d0311550c914315798821f6a397f53ac760919826a19af4",
+         arm64_linux:  "0d6d488851745e80e69b8935d063e742945811b47111994b1a6dbd27df3010d5",
+         x86_64_linux: "946cd06258d0ede72d0311550c914315798821f6a397f53ac760919826a19af4"
 
   url "https://storage.googleapis.com/antigravity-public/antigravity-cli/#{version.csv.first}-#{version.csv.second}/linux-#{arch}/cli_linux_#{(arch == "arm") ? "arm64" : "x64"}.tar.gz",
       verified: "storage.googleapis.com/antigravity-public/antigravity-cli/"
